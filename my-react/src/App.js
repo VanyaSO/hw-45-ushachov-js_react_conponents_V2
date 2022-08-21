fb239d7be769aa39809a8cd48f520235ab97e5fe
Ivan Ushachov <90464856+VanyaSO@users.noreply.github.com>
2022-08-21 13:49:15 +0300

    last

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -4,8 +4,8 @@ import Props from "./components/Props";
 function App() {
   return (
       <div className="App">
-        <Props title={"Заголовок"} text={null}/>
-        <Props title={null} text={"Тут просто текст"}/>
+        <Props title={"Заголовок"}/>
+        <Props text={"Тут просто текст"}/>
         <Props title={"Заголовок"} text={"Тут просто текст"}/>
       </div>
   );
